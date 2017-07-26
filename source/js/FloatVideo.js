88479f6c62484df565c642a27c44c9df4d400995
Elena Kuklina <e.kuklina@comradeweb.com>
2017-07-26 10:55:33 +0300

    Animation

--- source/js/FloatVideo.js
+++ source/js/FloatVideo.js
@@ -1,7 +1,76 @@
 (function () {
 	document.addEventListener("DOMContentLoaded", function () {
 
-		//module
+		//module Animate
+		const Animate = {
+			init(w) {
+				this.duration = 1000;
+				this.w = w;
+			},
+
+			run(options) {
+				//options = {duration, timing(timeFraction), draw}
+				options.duration = Animate.duration;;
+				var start = performance.now();
+
+				var animationFrame;
+
+				this.animationFrame = requestAnimationFrame(function animate(time) {
+					var timeFraction = (time - start) / options.duration;
+					if (timeFraction > 1) timeFraction = 1;
+
+					var progress = timeFraction;
+
+					options.draw(progress);
+
+					if (timeFraction < 1) {
+						requestAnimationFrame(animate);
+					}
+				});
+			},
+
+			bottom(el) {
+				var defW = el.clientWidth;
+
+				el.style.position = "fixed";
+
+				Animate.run({
+					draw: function (progress) {
+						el.style.bottom = (100 - (progress * 100)) + "%";
+						el.style.transform = "scale(" + (1 - (progress * (1 - Animate.w / defW))) + ")";
+					}
+				});
+			},
+
+			top(el) {
+				//if width=100%
+				var defW = document.body.clientWidth;
+
+				Animate.run({
+					draw: function (progress) {
+						el.style.top = 0;
+						el.style.bottom = "auto";
+						el.style.transform = "scale(" + ((Animate.w / defW) + (progress * (1 - Animate.w / defW))) + ")";
+					}
+				});
+			},
+
+			close(el) {
+				var defW = el.clientWidth;
+
+				Animate.run({
+					draw: function (progress) {
+						el.style.transform = "scale(" + (1 - (progress * (1 - Animate.w / defW))) + ")";
+					}
+				});
+			},
+
+			cancelAnimation() {
+				cancelAnimationFrame(Animate.animationFrame);
+			}
+		};
+
+		//module Video
 		const VideoFloat = {
 			init(containerID) {
 				this.container = document.getElementById("video-container");  //block with the video
@@ -21,75 +90,59 @@
 			},
 
 			coordinates() {
-				//point(Y), below which appears the fixed block = the lower boundary of main container with the video
-				return VideoFloat.container.parentElement.getBoundingClientRect().bottom;
+				return VideoFloat.container.parentElement.getBoundingClientRect();
 			},
 
-			create(className, w, h) { //"w" and "h" are numbers, not strings!
-				this.w = w;
-				this.h = h;
-
+			create(className) {
 				if (!VideoFloat.container.classList.contains(className)) {
 					VideoFloat.container.classList.add(className);
-					VideoFloat.container.style.width = this.w + "px";
-					VideoFloat.container.style.height = this.h + "px";
-
-					//overlay
 					VideoFloat.overlay.style.display = "block";
-
-					//VideoFloat.animate("bottom");
+					Animate.bottom(VideoFloat.container);
 				}
 			},
 
 			remove(className) {
 				if (VideoFloat.container.classList.contains(className)) {
 					VideoFloat.container.classList.remove(className);
-					VideoFloat.container.style = "";
-					VideoFloat.video.classList.remove("popup");
-					VideoFloat.overlay.dataset.popup = 0;
-					VideoFloat.overlay.style = "";
-
-					//overlay
 					VideoFloat.overlay.style.display = "none";
-
-					//VideoFloat.animate("top");
-				}
-			},
-
-			animate(vector) {
-				switch (vector) {
-					case "top":
-						break;
-					case "bottom":
-						break;
+					Animate.top(VideoFloat.container);
+					
+					if(VideoFloat.overlay.dataset.popup == 1) {
+						VideoFloat.video.classList.remove("popup");
+						VideoFloat.overlay.dataset.popup = 0;
+						VideoFloat.overlay.style = "";
+						//stop animate
+						Animate.cancelAnimation();
+					}	
 				}
 			},
 
 			popup(flag) {
 				switch(flag) {
 					case "open": 
-						up(1, "-1", 0.5, 0, 0, "100%", "100%");
+						toOverlay(1, "-1", 0.5, "100%");
+						Animate.top(VideoFloat.container);
 						VideoFloat.video.classList.add("popup");
 						break;
 					case "close": 
-						up(0, 0, 0, "auto", "auto", this.w + "px", this.h + "px");
+						toOverlay(0, 0, 0, "auto");
+						Animate.close(VideoFloat.container);
 						VideoFloat.video.classList.remove("popup");
+						VideoFloat.container.style.bottom = 0;
 						break;
 				}
 
-				function up(ds, z, op, t, l, w, h) {
+				function toOverlay(ds, z, op, ch) {
 					VideoFloat.overlay.dataset.popup = ds;
 					VideoFloat.overlay.style.zIndex = z;
 					VideoFloat.overlay.style.opacity = op;
-					VideoFloat.container.style.top = t;
-					VideoFloat.container.style.left = l;
-					VideoFloat.container.style.width = w;
-					VideoFloat.container.style.height = h;
+					VideoFloat.container.style.height = ch;
 				}
-
 			},
 
-			close() { }
+			static() {
+				VideoFloat.container.style.position = "static";
+			}
 		};
 
 
@@ -98,16 +151,16 @@
 
 		//page scrolling
 		document.addEventListener("scroll", function () {
-			var coord = VideoFloat.coordinates();
-			var w = 200;
-			var h = 100;
+			var bottom = VideoFloat.coordinates().bottom;
+			var top    = VideoFloat.coordinates().top;
+			var w      = 200; //min width fot container
 
-			if (coord <= 0) {
-				VideoFloat.create("fix", w, h);
-			}
-			else {
-				VideoFloat.remove("fix");
-			}
+			Animate.init(w);
+
+			if (bottom <= 0) VideoFloat.create("fix", w);
+			else VideoFloat.remove("fix");
+
+			if(top >= 0) VideoFloat.static();
 		});
 	});
 })();
